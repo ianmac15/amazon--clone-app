ea2efc9d71ae3c7fb9091893bbfd279ae65bb31e
ianmac15 <3thita@gmail.com>
2022-05-15 21:39:19 +0300

    1

--- amazon-clone-app/src/components/UserLogin.tsx
+++ amazon-clone-app/src/components/UserLogin.tsx
@@ -1,21 +1,43 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { newUserType } from './Users'
 
-const UserLogin = () => {
+const UserLogin = ({onAdd}:properties) => {
 
-    const [email, setEmail] = useState('')
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+    const [newUser, setNewUser] = useState<newUserType>(
+        {
+            email:'',
+            username:'',
+            password:''
+        })
+   
 
     const onSubmit = (e:React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault()
 
-        if(!email) {
+        if(!newUser.email) {
             alert('Please add an email')
             return
         }
 
-        setEmail(email)
+        if(!newUser.username) {
+            alert('Please add an email')
+            return
+        }
+
+        if(!newUser.password) {
+            alert('Please add an email')
+            return
+        }
+
+        
+
+        setNewUser({
+            email:'',
+            username:'',
+            password:''
+        })
+    
 
 
     }
@@ -26,8 +48,8 @@ const UserLogin = () => {
             <div className='userLogin'>
                 <h1>Sign In</h1>
                 <p>Enter email or phone number</p>
-                <form >
-                    <input type='text' value={email} placeholder='Example user@gmail.com'
+                <form onSubmit={onSubmit}>
+                    <input type='text' value={newUser.email} placeholder='Example user@gmail.com'
                         onChange={(e) => e.target.value} />
                     <input className='submit' type='submit' value='Continue' />
                 </form>
@@ -37,6 +59,10 @@ const UserLogin = () => {
             <Link className="btn register" to={"/register"}>Create your account</Link>
         </div>
     )
+}
+
+type properties = {
+    onAdd:
 }
 
 export default UserLogin
